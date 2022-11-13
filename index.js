c4b73100309dc92784c83a1e0e05770344ba2f47
Charlie Joseph <charliejgy4@gmail.com>
2022-11-13 14:08:40 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+
+
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.render('index1');
+});
+app.listen(3000, function () {
+  console.log('listening on port 3000!')
+});
+
+  app.set('view engine', 'ejs');
+
+  app.get("/", function(req, res) {    
+    res.render('index1')
+  });
+  app.use(express.static("public"));
+
+  var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var task = ["buy socks", "practise with nodejs", "learn Express todoapp"];
+
+app.post('/addtask', function (req, res) {
+    var newTask = req.body.newtask;
+
+    task.push(newTask);
+
+    res.redirect("/");
+});
+
+app.get("/", function(req, res) {
+  res.render('index1', { task: task, complete: complete});
+});
+
+
+var complete = ["finish buy stock"];
+app.post("/removetask", function(req, res) {
+     var completeTask = req.body.check;
+
+if (typeof completeTask === "string") {
+     complete.push(completeTask);
+
+     
+  task.splice(task.indexOf(completeTask), 1);
+} else if (typeof completeTask === "object") {
+    for (var i = 0; i < completeTask.length; i++) {     complete.push(completeTask[i]);
+    task.splice(task.indexOf(completeTask[i]), 1);
+}
+}
+   res.redirect("/");
+});
+  
+  
